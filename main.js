822610ab68c2216f20d6dcf0d274a3c7d3850302
David Everett <davideverett1989@gmail.com>
2020-01-28 01:21:05 -0600

    Sorting and expelling functions worked out.

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const showForm = () => {
-    let element = document.getElementById("name-form");
+    const element = document.getElementById("name-form");
     element.classList.toggle("disappear");
+}
+
+const clickListener = () => {
+    let DOMarray = document.getElementsByClassName("expel-button");
+    for (let i = 0; i < DOMarray.length; i++) {
+        DOMarray[i].addEventListener("click", removeStudent);
+    }
 }
 
 const printToDOM = (name, house) => {
@@ -9,10 +16,11 @@ const printToDOM = (name, house) => {
             <div class="card-body body-card">
                 <h5 class="card-title new-name">${name}</h5>
                 <p class="card-text new-house ">${house}</p>
-                <button id="expel-button" class="btn btn-primary expel-button">Expel</button>
+                <button class="btn btn-primary expel-button">Expel</button>
             </div>
         </div>`;
     document.getElementById("student-name").value = "";
+    clickListener();
 }
 
 const sortStudent = () => {
@@ -22,22 +30,15 @@ const sortStudent = () => {
     (studentName === "" || studentName === " ") ? alert("Please enter a name.") : printToDOM(studentName, houses[randomNum]);
 }
 
-const removeStudent = () => {
-    this.parentElement.parentNode.classList.add("disappear");
+const removeStudent = (e) => {
+    const element = e.path[2];
+    element.parentNode.removeChild(element);
 }
-
-const clickListener = () => {
-    let DOMarray = document.getElementsByClassName("expel-button");
-    for (let i = 0; i < DOMarray.length; i++) {
-        DOMarray[i].addEventListener("click", removeStudent);
-        console.log(DOMarray[i].parentElement.parentNode);
-    }
-}
-
 
 document.getElementById("show-form").addEventListener("click", showForm);
 document.getElementById("sort-student").addEventListener("click", function(x) {
     x.preventDefault();
     sortStudent();
 });
+
 
